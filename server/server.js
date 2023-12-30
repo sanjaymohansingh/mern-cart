2f7f24903976fdf8ff7081ee9a3331cafb167286
sanjaymohansingh <sanjaysingh19p@gmail.com>
2023-12-30 07:07:58 -0800

    connect to database

--- server/server.js
+++ server/server.js
@@ -1,6 +1,17 @@
 const express = require("express");
+const mongoose = require("mongoose");
+const dotenv = require("dotenv").config();
 
 const app = express();
+
+mongoose
+  .connect(process.env.CONNECTION_STRING)
+  .then(() => {
+    console.log("Connected to MongoDB");
+  })
+  .catch((err) => {
+    console.log(err);
+  });
 
 app.listen(5000, () => {
   console.log("Server is running on port 5000");
