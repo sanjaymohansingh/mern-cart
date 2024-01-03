b2b6bf1786921e119fb76c3981b5f9aa23379fa5
sanjaymohansingh <sanjaysingh19p@gmail.com>
2024-01-02 23:52:14 -0800

    correcting mongoose import

--- server/models/userModel.js
+++ server/models/userModel.js
@@ -1,22 +1,25 @@
-import mongoose from "mongoose";
+const mongoose = require("mongoose");
 
-const userSchema = new mongoose.Schema({
+const userSchema = new mongoose.Schema(
+  {
     username: {
-        type: String,
-        required: true,
-        unique : true,
+      type: String,
+      required: true,
+      unique: true,
     },
     email: {
-        type : String,
-        required : true,
-        unique : true,
+      type: String,
+      required: true,
+      unique: true,
     },
-    password : {
-        type : String,
-        required : true
-    }
-}, {timestamps : true});
+    password: {
+      type: String,
+      required: true,
+    },
+  },
+  { timestamps: true }
+);
 
 const User = mongoose.model("User", userSchema);
 
-module.exports = User
+module.exports = User;
